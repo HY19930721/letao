2e12d025450badec158039d3204907fa27fba8d9
HY19930721 <1765664092@qq.com>
2021-01-25 15:09:28 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    //表单校验插件，在表单提交的时候做校验，如果校验失败了，会阻止表单的提交，如果校验成功了，他就会让表单继续提交
     var $form =$("form");
     //初始化表单校验插件
     $form.bootstrapValidator({
@@ -20,13 +21,19 @@ $(function(){
                     notEmpty:{
                         //提示信息
                         message:"用户名不能为空哦"
+                    },
+                    callback:{
+                        message:"用户名错误"
                     }
                 }
             },
             password:{
-                validaators:{
+                validators:{
                     notEmpty:{
                         message:"密码不能为空哦"
+                    },
+                    callback:{
+                        message:"密码错误"
                     },
                     stringLength:{
                         min:6,
@@ -35,6 +42,48 @@ $(function(){
                     }
                 }
             }
-        }
+        },
+    });
+
+    //需要给表单注册一个校验成功的事件，这个事件在表单校验成功的时候会触发
+    $form.on("success.form.bv",function(e){
+        //阻止表单提交
+        e.preventDefault();
+        //发生Ajax请求
+        $.ajax({
+            type:"post",
+            url:"/employee/employeeLogin",
+            //注意：dataType:jquery会根据后端返回的类型自动识别
+            //content-type:text/json     text/html
+            //dataType:"json",
+            data:$form.serialize(),
+            success:function(info){
+                if(info.success){
+                    //跳转到index页面
+                    location.href = 'index.html';
+                }
+                if(info.error === 1000){
+                    //使用updateStatus方法，把用户名改成失败即可
+                    // $form.data("bootstrapValidator")获取对象插件实例 通过这个实例可以调用方法
+                    //3个参数：
+                    //1.字段名。。。。。。。。。。。。。username
+                    //2.状态 ：VALID   INVALID
+                    //3.显示哪个校验内容
+                    $form.data("bootstrapValidator").updateStatus("username","INVALID","callback");
+                    
+                    //alert(info.message);
+                }
+                if(info.error === 1001){
+                    $form.data("bootstrapValidator").updateStatus("password","INVALID","callback");
+                    //alert(info.message);
+                }
+            },
+        });
+    });
+
+
+    //给reset按钮注册一个点击事件
+    $("[type ='reset']").on("click",function(){
+        $form.data("bootstrapValidator").resetForm();
     });
 });
